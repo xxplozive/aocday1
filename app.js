a1e82262a7ca744b6a26bd9b578e0dd685d2a38b
Kartik Nath <kartiknath@gmail.com>
2022-01-15 21:50:59 -0500

    progress but not quite there yet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 
-fs.readFile('/Users/kartiknath/Documents/code/aocday1/inputsample.txt', 'utf8' , (err, data) => {
+const pathMac = '/Users/kartiknath/Documents/code/aocday1/inputsample.txt';
+const pathWin = 'c:\\code\\aocday1\\inputsample.txt'
+fs.readFile(pathWin, 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
@@ -12,18 +14,22 @@ fs.readFile('/Users/kartiknath/Documents/code/aocday1/inputsample.txt', 'utf8' ,
   //}
   
   function sum3nums(a, b, c) {return a + b + c};
-  
-
-  for (let i=0; i < 1; i++){
+  const arraySums = [];
+  var numberSumIncreases = 0; 
+  for (let i=0; i < array.length-3 ; i++){
     var totalNumber1 = sum3nums(array[i], array[i+1], array[i+2]);
-    
+    var totalNumber2 = sum3nums(array[i+1], array[i+2], array[i+3]);
+    //console.log(`The first group total is ${totalNumber1}`);
+    //console.log(`The second group total is ${totalNumber2}`);
+    arraySums.push(totalNumber1);
+    arraySums.push(totalNumber2);
+    if (totalNumber2 > totalNumber1){
+      numberSumIncreases++;
+    }
   };
 
-
-  console.log(totalNumber1);
-
-
+  //console.log(`The total number of sums that are greater than the previous one is ${numberSumIncreases}`);
+  console.log(arraySums);
+  
  // console.log(totalNumber);
-
-
 });
