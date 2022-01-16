a6994f2253dc704194c8e97e38418651c6690937
Kartik Nath <kartiknath@gmail.com>
2022-01-16 16:30:41 -0500

    final answer

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 const fs = require('fs')
 
 const pathMac = '/Users/kartiknath/Documents/code/aocday1/inputsample.txt';
-const pathWin = 'c:\\code\\aocday1\\inputsample.txt'
+const pathWin = 'c:\\code\\aocday1\\input.txt'
 fs.readFile(pathWin, 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
   }
   var array = data.toString().split("\n").map(Number);
-  
-  //for(i in array){
-  //console.log(array[i]);
-  //}
-  
+  let remain = array.length % 3; 
   function sum3nums(a, b, c) {return a + b + c};
   const arraySums = [];
   var numberSumIncreases = 0; 
-  for (let i=0; i < array.length-3 ; i++){
-    var totalNumber1 = sum3nums(array[i], array[i+1], array[i+2]);
-    var totalNumber2 = sum3nums(array[i+1], array[i+2], array[i+3]);
-    //console.log(`The first group total is ${totalNumber1}`);
-    //console.log(`The second group total is ${totalNumber2}`);
-    arraySums.push(totalNumber1);
-    arraySums.push(totalNumber2);
-    if (totalNumber2 > totalNumber1){
+  for (let i=0; i < array.length; i++){
+    var totalNumber = sum3nums(array[i], array[i+1], array[i+2]);
+    arraySums.push(totalNumber);
+  };
+  
+  for (let j = 0; j < arraySums.length; j++) {
+    if(arraySums[j] < arraySums[j+1]){
       numberSumIncreases++;
     }
   };
-
-  //console.log(`The total number of sums that are greater than the previous one is ${numberSumIncreases}`);
-  console.log(arraySums);
   
- // console.log(totalNumber);
+  //console.log(array.length);
+  //console.log (arraySums.length);
+  //console.log(arraySums);
+  //console.log(remain);
+  // console.log(totalNumber);
+  console.log(`The total number of sums that are greater than the previous one is ${numberSumIncreases}`);
 });
+
+// The answer is 1575
